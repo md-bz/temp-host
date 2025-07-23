010649e0467f4b45888e195873b87e6ca4fa4dd1
mehrad <mmfirst@protonmail.com>
2025-07-23 12:55:20 +0330

    refactor: refactored get/save file logic for reusability

--- src/api.ts
+++ src/api.ts
@@ -1,65 +1,35 @@
 import { Hono } from "hono";
-import * as fs from "fs/promises";
-import { join } from "path";
-import { db } from "./db/db";
-import { files } from "./db/schema";
-import { nanoid } from "nanoid";
-import { eq } from "drizzle-orm";
-import { toUnixDate } from "./helper";
+import { getFile, HttpError, saveFile } from "./helper";
 
 export const api = new Hono();
-
-const maxAge = toUnixDate(process.env.MAX_AGE || "1d");
 
 api.post("/file", async (c) => {
     const body = await c.req.parseBody();
 
-    const file = body["file"] as File;
-    const age = body["age"] as string;
-
-    if (!file) {
-        c.json({ error: "no file sent" }, 400);
-    }
-
-    const data = await file.bytes();
-    const storagePath = join("./", "files", file.name + Date.now());
     try {
-        await fs.writeFile(storagePath, data);
-    } catch (err) {
-        console.error(err);
+        const url = await saveFile(body);
+        return c.json({ url });
+    } catch (error) {
+        if (error instanceof HttpError) {
+            return c.json({ error: error.message }, error.status);
+        }
         return c.json({ error: "something went wrong" }, 500);
     }
-
-    const url = nanoid();
-
-    await db.insert(files).values({
-        filename: file.name,
-        size: file.size,
-        expiresAt: Date.now() + Math.min(maxAge, toUnixDate(age, Infinity)),
-        url,
-        storagePath: storagePath,
-    });
-
-    return c.json({ url });
 });
 
 api.get("/file/:id", async (c) => {
     const id = c.req.param("id");
-    const file = await db.select().from(files).where(eq(files.url, id)).get();
 
-    if (!file || file.expiresAt < Date.now()) {
-        return c.json({ error: "file not found" }, 404);
-    }
-
-    let data;
     try {
-        data = await fs.readFile(file.storagePath);
+        const { data, filename } = await getFile(id);
+        return c.body(data, 200, {
+            "Content-Type": "application/octet-stream",
+            "Content-Disposition": `attachment; filename="${filename}"`,
+        });
     } catch (error) {
+        if (error instanceof HttpError) {
+            return c.json({ error: error.message }, error.status);
+        }
         return c.json({ error: "something went wrong" }, 500);
     }
-
-    return c.body(data, 200, {
-        "Content-Type": "application/octet-stream",
-        "Content-Disposition": `attachment; filename="${file.filename}"`,
-    });
 });
--- src/helper.ts
+++ src/helper.ts
@@ -1,3 +1,11 @@
+import * as fs from "fs/promises";
+import { nanoid } from "nanoid";
+import { db } from "./db/db";
+import { files } from "./db/schema";
+import { join } from "path";
+import { ContentfulStatusCode } from "hono/utils/http-status";
+import { eq } from "drizzle-orm";
+
 export function toUnixDate(
     input: string | number,
     defaultTime: number = 0
@@ -31,4 +39,63 @@ export function toUnixDate(
     }
 
     return defaultTime;
+}
+
+const maxAge = toUnixDate(process.env.MAX_AGE || "1d");
+
+export class HttpError extends Error {
+    status: ContentfulStatusCode;
+
+    constructor(message: string, status: ContentfulStatusCode) {
+        super(message);
+        this.status = status;
+    }
+}
+
+export async function saveFile(body: { [key: string]: string | File }) {
+    const file = body["file"] as File;
+    const age = body["age"] as string;
+
+    if (!file) {
+        throw new HttpError("no file sent", 400);
+    }
+
+    const data = await file.bytes();
+    const storagePath = join("./", "files", file.name + Date.now());
+    try {
+        await fs.writeFile(storagePath, data);
+    } catch (err) {
+        console.error(err);
+        throw new HttpError("something went wrong", 500);
+    }
+
+    const url = nanoid();
+
+    await db.insert(files).values({
+        filename: file.name,
+        size: file.size,
+        expiresAt: Date.now() + Math.min(maxAge, toUnixDate(age, Infinity)),
+        url,
+        storagePath: storagePath,
+    });
+
+    return url;
+}
+
+export async function getFile(id: string) {
+    const file = await db.select().from(files).where(eq(files.url, id)).get();
+
+    if (!file || file.expiresAt < Date.now()) {
+        throw new HttpError("file not found", 404);
+    }
+
+    let data;
+    try {
+        data = await fs.readFile(file.storagePath);
+    } catch (error) {
+        console.log(error);
+        throw new HttpError("something went wrong", 500);
+    }
+
+    return { data, filename: file.filename };
 }
