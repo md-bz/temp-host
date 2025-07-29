19cf368bcd968483871e14ff3efcae3cbf9b5376
mehrad <mmfirst@protonmail.com>
2025-07-29 13:54:19 +0330

    feat: added skiping ratelimiter via env var

--- src/ratelimter.tsx
+++ src/ratelimter.tsx
@@ -2,6 +2,7 @@ import RedisStore, { RedisReply } from "rate-limit-redis";
 import RedisClient from "ioredis";
 import { rateLimiter, Store } from "hono-rate-limiter";
 import { toUnixDate } from "./helper";
+import { createMiddleware } from "hono/factory";
 
 if (!process.env.REDIS_URL) {
     throw new Error("REDIS_URL environment variable is not set");
@@ -17,28 +18,30 @@ const store = new RedisStore({
 }) as unknown as Store;
 
 export const limiter = (type: "json" | "html" = "html") =>
-    rateLimiter({
-        windowMs: toUnixDate(process.env.RATE_LIMIT_WINDOW || "15m"),
-        limit: process.env.RATE_LIMIT_COUNT
-            ? Number(process.env.RATE_LIMIT_COUNT)
-            : 10,
-        handler: (c) => {
-            if (type === "html")
-                return c.render(
-                    <h2>Too many requests, please try again later.</h2>,
-                    429
-                );
-            return c.json(
-                { error: "Too many requests, please try again later." },
-                429
-            );
-        },
-
-        keyGenerator: (c) => {
-            const jwt = c.get("jwtPayload");
-            if (jwt) {
-                return jwt.id;
-            }
-        },
-        store,
-    });
+    process.env.RATE_LIMIT_SKIP === "true"
+        ? createMiddleware((c, next) => next())
+        : rateLimiter({
+              windowMs: toUnixDate(process.env.RATE_LIMIT_WINDOW || "15m"),
+              limit: process.env.RATE_LIMIT_COUNT
+                  ? Number(process.env.RATE_LIMIT_COUNT)
+                  : 10,
+              handler: (c) => {
+                  if (type === "html")
+                      return c.render(
+                          <h2>Too many requests, please try again later.</h2>,
+                          429
+                      );
+                  return c.json(
+                      { error: "Too many requests, please try again later." },
+                      429
+                  );
+              },
+              keyGenerator: (c) => {
+                  const jwt = c.get("jwtPayload");
+                  if (jwt) {
+                      return jwt.id;
+                  }
+                  return "everyone";
+              },
+              store,
+          });
